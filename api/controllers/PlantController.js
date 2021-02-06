002bead1306d4d07561f28b07a53f4e660cf7620
Winithepooh <imbatman@wp.pl>
2021-02-06 10:00:45 +0100

    feature/plant-create-form - plant create form, wip: submiting

--- api/controllers/PlantController.js
+++ api/controllers/PlantController.js
@@ -6,28 +6,30 @@
  */
 
 module.exports = {
-  dispatch: function(req,res){
+  dispatch: function (req, res) {
     res.send('hello world!');
   },
 
-  GetUserPlants: async function(req,res){
-    let user = await User.find({name:"Wojtek"}) // TODO: Make is dynamic
-    let robots = await Robot.find({userId: user[0].id})
-    let plants = await Plant.find({robotId: robots[0].id}) // TODO: Make it so less DB querries are needed. Maybe keep ID?
-    res.view("pages/flowers_panel", {
+  GetUserPlants: async function (req, res) {
+    let user = await User.find({name: 'Wojtek'}); // TODO:Make is dynamic
+    let robots = await Robot.find({userId: user[0].id});
+    let plants = await Plant.find({robotId: robots[0].id}); // TODO: Make it so less DB querries are needed. Maybe keep ID?
+    res.view('pages/flowers_panel', {
       plants: plants
     });
   },
   GetCreatePlantForm: async function (req, res) {
-    res.view('pages/robot_create');
+    // let user = await User.find({name: 'Wojtek'}); // TODO:Make is dynamic
+    let robots = Robot.find({id: 1});
+    res.view('pages/plant_create', {viewPageTitle: 'Nowa roślina', robots: robots});
   },
 
   Save: async function (req, res) {
     Plant.create({
-        robotId: req.robot,
-        name: req.name,
-        type: req.type,
-        image: req.image,
+      robotId: req.robot,
+      name: req.name,
+      type: req.type,
+      image: req.image,
     });
     return 'success';
   }
--- api/controllers/RobotController.js
+++ api/controllers/RobotController.js
@@ -8,7 +8,7 @@
 module.exports = {
 
   GetUserRobots: async function (req, res) {
-    let user = await User.find({name: 'Wojtek'}); // TODO: Make is dynamic
+    let user = await User.find({name: 'test'}); // TODO: Make is dynamic
     let robots = await Robot.find({userId: user[0].id});
     res.view('pages/robot_panel', {
       robots: robots
